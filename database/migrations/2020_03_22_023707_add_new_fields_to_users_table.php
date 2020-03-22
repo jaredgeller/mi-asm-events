97d59a081259ac48e3397b0b85fab8189944a607
Jared Geller <11000920+jaredgeller@users.noreply.github.com>
2020-03-22 00:08:57 -0400

    fix rename col

--- database/migrations/2020_03_22_023707_add_new_fields_to_users_table.php
+++ database/migrations/2020_03_22_023707_add_new_fields_to_users_table.php
@@ -14,8 +14,7 @@ class AddNewFieldsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('name', 'first_name');
-            $table->string('last_name', 50)->after('first_name');
+            $table->string('last_name', 50)->after('name');
             $table->string('address_street_1', 200)->after('password')->nullable();
             $table->string('address_street_2', 200)->after('address_street_1')->nullable();
             $table->string('address_city', 200)->after('address_street_2')->nullable();
@@ -23,6 +22,8 @@ class AddNewFieldsToUsersTable extends Migration
             $table->string('address_zip', 10)->after('address_state')->nullable();
             $table->tinyInteger('administrator_ind')->default(0)->after('address');
             $table->date('mi_member_date')->after('administrator_ind')->nullable();
+
+            $table->renameColumn('name', 'first_name');
         });
     }
 
