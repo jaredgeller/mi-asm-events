5bf4dba3ef962d86b94647f6edb7cfa8c5a4e841
Jared Geller <11000920+jaredgeller@users.noreply.github.com>
2020-03-22 11:53:24 -0400

    add instructor seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,13 @@ class DatabaseSeeder extends Seeder
             'first_name' => 'Jared',
             'last_name' => 'Geller',
             'email' => 'jgeller@email.davenport.edu',
+            'password' => Hash::make('strongestavenger'),
+            'administrator_ind' => 1,
+        ]);
+        DB::table('users')->insert([
+            'first_name' => 'Loay',
+            'last_name' => 'Alnaji',
+            'email' => 'lalnaji@davenport.edu',
             'password' => Hash::make('allthestars'),
             'administrator_ind' => 1,
         ]);
