5c3f9948a5f3b103d0f6923a89220f64be268249
Jared Geller <11000920+jaredgeller@users.noreply.github.com>
2020-03-22 22:58:07 -0400

    placeholder create event button

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -41,6 +41,12 @@
                                 </tbody>
                             </table>
                         </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <a href="#" class="btn btn-primary">Create Event</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
