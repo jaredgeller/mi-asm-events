fa3c005f7a09062bcc0aaefc855b358d5bf629eb
Jared Geller <11000920+jaredgeller@users.noreply.github.com>
2020-03-23 10:23:10 -0400

    send events to page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', [
+            'events' => Event::all(),
+        ]);
     }
 
     /**
